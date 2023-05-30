f1d68a07c30efb34ebdd5d6cd551442699ed07eb
André Hopf <andre.hopf@gmail.com>
2023-05-30 14:05:21 -0300

    Adiciona estrutura inicial do projeto spring

---
+++ src/test/java/com/devinhouse/labsky/LabskyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinhouse.labsky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabskyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
