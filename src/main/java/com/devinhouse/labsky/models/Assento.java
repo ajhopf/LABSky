03de24d0c097dd6aecd767dfe30b9a9bd82d08e7
André Hopf <andre.hopf@gmail.com>
2023-05-31 11:37:15 -0300

    Cria entidade Assento e adiciona assentos ao script data.sql

---
+++ src/main/java/com/devinhouse/labsky/models/Assento.java
@@ -0,0 +1,21 @@
+package com.devinhouse.labsky.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "assentos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Assento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String assento;
+    @Column(nullable = false, name = "fileira_de_emergencia")
+    private char fileiraDeEmergencia;
+}
